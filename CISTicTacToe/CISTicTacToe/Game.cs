cde3fa6bcd2d8cda77b06ed92d81d163d038d803
wyqtpu <wuyiquntpu@gmail.com>
2019-10-23 21:46:04 +0800

    #add all

---
+++ CISTicTacToe/CISTicTacToe/Game.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CISTicTacToe
+{
+    public class Game
+    {
+        public char[,] board;
+        public char player;
+
+        public Game()
+        {
+            board = new char[3, 3];
+            player = 'X';
+        }
+
+        public char[,] CreateBoard()
+        {
+            board = new char[3, 3];
+            player = 'X';
+
+            return board;
+        }
+
+        public char CurrentPlayer()
+        {
+            return player;
+        }
+
+        public char ChangePlayer()
+        {
+            if (player == 'X')
+                player = 'O';
+            else
+                player = 'X';
+            return player;
+        }
+
+        public void MakeMove(int x, int y)
+        {
+            if (!SpaceInUse(x, y))
+            {
+                board[x, y] = player;
+                ChangePlayer();
+            }
+        }
+
+        public bool SpaceInUse(int x, int y)
+        {
+            return (board[x, y] == 'X' || board[x, y] == 'O');
+        }
+    }
+}
+
---
+++ CISTicTacToe/CISTicTacToeTests/Tests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CISTicTacToe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CISTicTacToe.Tests
+{
+    [TestClass()]
+    public class GameTests
+    {
+        [TestMethod()]
+        public void CreateBoardTest()
+        {
+            Game salt = new Game();
+            int count = 0;
+            char[,] toTest = salt.CreateBoard();
+
+            for (int i = 0; i < toTest.GetLength(0); i++)
+            {
+                for (int j = 0; j < toTest.GetLength(1); j++)
+                {
+                    count++;
+                }
+            }
+
+            //Assert the board has 9 active fields
+            Assert.AreEqual(count, 9);
+        }
+
+        [TestMethod()]
+        public void CurrentPlayerTest()
+        {
+            Game board1 = new Game();
+            board1.CreateBoard();
+            char toTest = board1.CurrentPlayer();
+
+            //Asser the current player is X
+            Assert.AreEqual('X', toTest);
+        }
+
+        [TestMethod()]
+        public void ChangePlayerTest()
+        {
+            Game board1 = new Game();
+            board1.CreateBoard();
+            char toTest = board1.ChangePlayer();
+
+            //Assert the current player is O
+            Assert.AreEqual('O', toTest);
+        }
+
+        [TestMethod()]
+        public void MakeMoveTest()
+        {
+            Game board1 = new Game();
+            char[,] boardToTest = board1.CreateBoard();
+            board1.MakeMove(1, 1);
+            char toTest = board1.board[1, 1];
+
+            //Assert that the current player places an X in a spot on the board
+            Assert.AreEqual('X', toTest);
+        }
+
+
+
+
+
+
+        [TestMethod()]
+        public void MakeMoveTest2()
+        {
+            Game board1 = new Game();
+            char[,] boardToTest = board1.CreateBoard();
+            board1.ChangePlayer();
+            board1.MakeMove(1, 1);
+            char toTest = board1.board[1, 1]; 
+            //Assert that the current player places an O in a spot on the board
+            Assert.AreEqual(toTest, 'O');
+        }
+
+
+        [TestMethod()]
+        public void SpaceInUseTest()
+        {
+            Game board1 = new Game();
+            board1.CreateBoard();
+            board1.MakeMove(1, 1);
+            board1.MakeMove(1, 1);
+            char toTest = board1.board[1, 1];
+
+            //Assert that the placement is an unoccupied spot
+            Assert.AreEqual('X', toTest);
+        }
+    }
+}
